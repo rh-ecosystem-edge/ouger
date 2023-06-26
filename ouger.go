3465030fe5af22c3018bd3ee9414bedd703e5842
Omer Tuchfeld <omer@tuchfeld.dev>
2023-06-26 14:45:23 +0200

    Protocol detection

--- ouger.go
+++ ouger.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 
 	"github.com/openshift/api"
 	yaml "gopkg.in/yaml.v2"
-	kapiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	jsonserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
+	kapiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 	"k8s.io/kubectl/pkg/scheme"
 )
 
 var ProtobufMediaType = "application/vnd.kubernetes.protobuf"
+
+var ProtoEncodingPrefix = []byte{0x6b, 0x38, 0x73, 0x00}
+
+func tryFindProto(in []byte) ([]byte, bool) {
+	i := bytes.Index(in, ProtoEncodingPrefix)
+	if i >= 0 && i < len(in) {
+		return in[i:], true
+	}
+	return nil, false
+}
 
 func main() {
 	stdin, err := ioutil.ReadAll(os.Stdin)
@@ -33,15 +44,20 @@ func main() {
 	builder.AddToScheme(scheme.Scheme)
 
 	if os.Args[1] == "decode" {
-		decoder := scheme.Codecs.UniversalDeserializer()
-		encoder := jsonserializer.NewYAMLSerializer(jsonserializer.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
-		obj, _, err := decoder.Decode(stdin, nil, nil)
-		if err != nil {
-			panic(err)
-		}
-		err = encoder.Encode(obj, os.Stdout)
-		if err != nil {
-			panic(err)
+		if _, ok := tryFindProto(stdin); ok {
+			decoder := scheme.Codecs.UniversalDeserializer()
+			encoder := jsonserializer.NewYAMLSerializer(jsonserializer.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
+
+			obj, _, err := decoder.Decode(stdin, nil, nil)
+			if err != nil {
+				panic(err)
+			}
+			err = encoder.Encode(obj, os.Stdout)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			os.Stdout.Write(stdin)
 		}
 	} else if os.Args[1] == "encode" {
 		typeMeta, err := typeMetaFromYaml(stdin)
